8bc3d7e923debb36ac6c7d0c6ce13a3923bc9d0c
Scorpsy <matthewraynard65@gmail.com>
2021-01-12 14:18:23 -0800

    package imports

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
+const cheerio = require('cheerio');
+const request = require('request');
+const fs = require('fs');
 
 const client = new Discord.Client();
 const prefix = '-';
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
     console.log('FoodBot is alive!');
@@ -12,11 +25,14 @@ client.once('ready', () => {
 client.on('message', message => {
     if(!message.content.startsWith(prefix) || message.author.bot) return; 
 
-    const args = message.content.slice(prefix.length).split(/ +/);
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
 
     if(command === 'ping'){
-        message.channel.send('pong!');
+        client.commands.get('ping').execute(message, args);
+    }
+    else if(command === 'gimage'){
+        client.commands.get('gimage').execute(message, args);
     }
 
 });
