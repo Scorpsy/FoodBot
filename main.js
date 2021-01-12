fb557ca9a284fe0c0c53f99e5779f16ce3ce143a
Scorpsy <matthewraynard65@gmail.com>
2021-01-12 10:15:46 -0800

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+const prefix = '-';
+
+client.once('ready', () => {
+    console.log('FoodBot is alive!');
+
+});
+
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return; 
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        message.channel.send('pong!');
+    }
+
+});
+
+client.login('Nzk4NTkyMDExODA0MzQ0Mzkw.X_3QxA.5YhLghR2oF8yve6amMCMjNp3CYI');
