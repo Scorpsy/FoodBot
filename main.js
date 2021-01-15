8b3d4af1dc2fed0c173d16bf67aebdac3a30ff0e
Scorpsy <matthewraynard65@gmail.com>
2021-01-15 02:43:02 -0800

    commented spolls because it keeps breaking

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ client.once('ready', () => {
 
 });
 
-client.on('message', message => {
+client.on('message', async(message) => {
     
     if(!message.content.startsWith(prefix) || message.author.bot) return; 
 
@@ -41,9 +41,9 @@ client.on('message', message => {
     else if(command === '8ball'){
         client.commands.get('8ball').execute(message, args);
     }
-    else if(command === 'spolls'){
-        client.commands.get('spolls').execute(message, args);
-    }
+    // else if(command === 'spolls'){
+    //     client.commands.get('spolls').execute(message, args);
+    // }
     else{
         message.channel.send('That response is curretly under development');
     }
