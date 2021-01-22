50e6e5e04573287355b3a8353a837921b5ee34ee
Scorpsy <matthewraynard65@gmail.com>
2021-01-21 20:55:26 -0800

    plan to remove

--- commands/fooders.js
+++ commands/fooders.js
@@ -1,30 +1,21 @@
 const squigglyRegex = RegExp(/{(.*?)}/);
 const squareRegex = RegExp(/\[[^[]+\]/g);
-const moment = require('moment');
-const ms = require('ms');
 const Discord = require('discord.js');
 
-module.exports = {
-    name: 'fooders',
-    description: 'create a list for delivery orders',
-    async execute(message,args, orders) {
+function gay(message, args){
 
-        const format = '{Title} [Option 1] [Option 2] [Option 3]';
-        const pollParameters = args.join(' ');
-        const orderTitle = squigglyRegex.test(pollParameters) ? squigglyRegex.exec(pollParameters)[1] : null;
-        console.log(squigglyRegex.exec(pollParameters));
+    const orderTitle = args.join(' ');
 
-
-        if (!orderTitle) {
-            return message.channel.send('Use format: ' + format).catch(err => console.log(err));
-        }
-
-        orders.setTitle(orderTitle);
-
-        message.channel.send('order for ' + orderTitle + ' created');
-        
-        return orders;
-
+    if (!orderTitle) {
+        return message.channel.send('Please add a restaurant name').catch(err => console.log(err));
     }
 
+    var orders = new Discord.MessageEmbed()
+            .setColor('#223441');
+
+    orders.setTitle(orderTitle);
+
+    message.channel.send('order for ' + orderTitle + ' created');
+
+    return orders;
 }
